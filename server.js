aaa211f04d8cdb274bb42bda57f7d5a3cf4c27a4
Brianna <briannaodell1437@gmail.com>
2022-08-15 20:35:13 -0700

    post route structure

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const express = require("express");
 // needs fs module
 const fs = require("fs");
 const path = require("path");
-const { clog } = require("./middleware/clog");
-const api = require("./routes/index.js");
 // uuid npm require
 const { v4: uuidv4 } = require("uuid");
 
@@ -12,15 +10,12 @@ const PORT = process.env.port || 3001;
 
 const app = express();
 
-// Import custom middleware, "cLog"
-app.use(clog);
-
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// need api tourtes
-app.use("/api", api);
+// need api routes
+//app.use("/api", api);
 
 app.use(express.static("public"));
 
@@ -46,25 +41,35 @@ app.get("/api/notes", (req, res) => {
   });
 });
 
-// TODO: GET /api/notes should receive a new note to save on request body, add to db.json, needs unique id (UUID) npm packages might be able to do this - post not get
+// TODO: GET /api/notes should receive a new note to save on request body, add to db.json, needs unique id (UUID) npm packages might be able to do this
+// post not get -- check activity 18
 app.post("/api/notes", (req, res) => {
-  fs.writeFile("./db/db.json", (err, data) => {
-    JSON.stringify(note);
+  // destructure req body
+  const { title, text } = req.body;
 
-    // destructure req body
-    const { title, text } = req.body;
-
-    // check that theres data
-    if (title && text) {
-      const postNote = {
-        title,
-        text,
-      };
-    }
-  });
+  // check that theres data, add id
+  if (title && text) {
+    const postNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+    // from mini project
+    fs.readFile(file, "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(postNote);
+        writeToFile("./db/db.json", parsedData);
+      }
+    });
+  }
 });
 
 // BONUS TODO: DELETE /api/notes/:id should receive query param that has id, read all notes from db.json file and remove the note with the id property and then rewrite to db.json
+
+app.delete;
 
 // shows that the app is listening at the port
 app.listen(PORT, () =>
