d07f947dc17e089130b1236470678b987ad0d9e1
Brianna <briannaodell1437@gmail.com>
2022-08-16 22:09:03 -0700

    review mini project, prep test fire

---
+++ routers/notes.js
@@ -0,0 +1,70 @@
+// https://www.youtube.com/watch?v=SccSCuHhOw0 - help with routes
+const express = require("express");
+const router = express.Router();
+// uuid npm require
+const { v4: uuidv4 } = require("uuid");
+const { readFromFile, writeToFile, readAndAppend } = require("./helpers/utils");
+
+// TODO: GET /api/notes should read the db.json and return saved notes as JSON
+router.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+// TODO: GET /api/notes should receive a new note to save on request body, add to db.json, needs unique id (UUID) npm packages might be able to do this
+// post not get -- check activity 18
+router.post("/api/notes", (req, res) => {
+  // destructure req body
+  const { title, text } = req.body;
+
+  // check that theres data, add id
+  if (title && text) {
+    const postNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+    // from mini project
+    readAndAppend(postNote, "./db/db.json");
+
+    const response = {
+      status: "success",
+      body: postNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json("Error in posting feedback");
+  }
+});
+
+// get specific notes - from mini project
+router.get("/:id", (req, res) => {
+  const notesId = req.params.id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id === notesId);
+      return result.length > 0
+        ? res.json(result)
+        : res.json("No note with that ID");
+    });
+});
+
+// BONUS TODO: DELETE /api/notes/:id should receive query param that has id, read all notes from db.json file and remove the note with the id property and then rewrite to db.json - check mini project
+router.delete("/id", (req, res) => {
+  const notesId = req.params.id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all tips except the one with the ID provided in the URL
+      const result = json.filter((note) => note.id !== notesId);
+
+      // Save that array to the filesystem
+      writeToFile("./db/db.json", result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${notesId} has been deleted 🗑️`);
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const express = require("express");
 // needs fs module
 const fs = require("fs");
 const path = require("path");
-// uuid npm require
-const { v4: uuidv4 } = require("uuid");
+const api = require("./routes/index.js");
 
 const PORT = process.env.port || 3001;
 const app = express();
 
+const notesRouter = require("./routers/notes");
+
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/notes", notesRouter);
 
 app.use(express.static("public"));
 
@@ -24,48 +26,6 @@ app.get("/notes", (req, res) =>
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
-
-// TODO: GET /api/notes should read the db.json and return saved notes as JSON
-app.get("/api/notes", (req, res) => {
-  fs.readFile("./db/db.json", (err, data) => {
-    if (err) {
-      console.log(error);
-    } else {
-      const savedNotes = JSON.parse(data);
-      res.json(savedNotes);
-    }
-  });
-});
-
-// TODO: GET /api/notes should receive a new note to save on request body, add to db.json, needs unique id (UUID) npm packages might be able to do this
-// post not get -- check activity 18
-app.post("/api/notes", (req, res) => {
-  // destructure req body
-  const { title, text } = req.body;
-
-  // check that theres data, add id
-  if (title && text) {
-    const postNote = {
-      title,
-      text,
-      id: uuidv4(),
-    };
-    // from mini project
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        var parsedData = JSON.parse(data);
-        parsedData.push(postNote);
-        fs.writeFile("./db/db.json", JSON.stringify(parsedData), (err) =>
-          err ? console.error(err) : console.info(`\nData written to db.json`)
-        );
-      }
-    });
-  }
-});
-
-// BONUS TODO: DELETE /api/notes/:id should receive query param that has id, read all notes from db.json file and remove the note with the id property and then rewrite to db.json
 
 // shows that the app is listening at the port
 app.listen(PORT, () =>
